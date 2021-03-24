befa194b15b064c7f19a7c708b5a5796ec877f3a
Rayan-Mansour <Rayan-m1999@hotmail.com>
2021-03-24 16:20:32 +0300

    abc

--- eStorePlatform/functions/Product_function.js
+++ eStorePlatform/functions/Product_function.js
@@ -1,15 +1,21 @@
-
-
-
-
+var pName = document.getElementById("pName");
+var pPrice = document.getElementById("pPrice");
+var pQuantity = document.getElementById("pQuantity");
+var pDesc = document.getElementById("desc");
+const sub = document.getElementById("submit");
+sub.addEventListener("submit" , (e) =>{
+  if(pPrice.value <=0 || pQuantity.value <=0){
+    alert("negative value not accepted");
+  e.preventDefault();
+  }
+})
 //var products = new Array();
 function AddProduct(){
   document.getElementById("addBox").style.visibility = "visible";
 
 }
-//function add(){
-//  document.getElementById("addBox").style.visibility = "visible";
-//}
+
+
 function cancel(){
   document.getElementById("addBox").style.visibility = "hidden";
 }
