4ee2eb4975acc45be0a45b39aac71667774bf6f5
Rayan-Mansour <Rayan-m1999@hotmail.com>
2021-03-24 19:31:01 +0300

    product updated

--- eStorePlatform/functions/Product_function.js
+++ eStorePlatform/functions/Product_function.js
@@ -2,12 +2,19 @@ var pName = document.getElementById("pName");
 var pPrice = document.getElementById("pPrice");
 var pQuantity = document.getElementById("pQuantity");
 var pDesc = document.getElementById("desc");
+var pImg = document.getElementById("img");
 const sub = document.getElementById("submit");
+
+
 sub.addEventListener("submit" , (e) =>{
   if(pPrice.value <=0 || pQuantity.value <=0){
     alert("negative value not accepted");
   e.preventDefault();
   }
+ else
+ var product = new Object();
+ product.name = pName;
+ 
 })
 //var products = new Array();
 function AddProduct(){
@@ -19,3 +26,4 @@ function AddProduct(){
 function cancel(){
   document.getElementById("addBox").style.visibility = "hidden";
 }
+
