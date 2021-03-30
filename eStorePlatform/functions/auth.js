7144272060174ac5074bfd24a9a9949a0e246ddf
Ayidh1 <rveercee.15@gmail.com>
2021-03-30 16:00:38 +0300

    front-end connected with back-end server finally...

--- eStorePlatform/functions/auth.js
+++ eStorePlatform/functions/auth.js
@@ -1,7 +1,9 @@
 
 const router = require('express').Router();
-const firebase = require('./firebase').default;
+const firebase = require('./firebase');
 const bodyparser = require('body-parser');
+const db = require('./database');
+
 
 
 router.use(bodyparser.urlencoded({extended: true}));
@@ -22,32 +24,39 @@ const auth = firebase.auth();
 router.post('/signup', (req, res) => {
 
 
+    const files = [];
     const firstname = req.body.firstname;
     const lastname = req.body.lastname;
     const pass = req.body.password;
     const email = req.body.email;
     const tele = req.body.telephone;
     const storename = req.body.store;
+    const image = req.body.imgg;
+    
+    files[0] = image;
 
     var id = 0;
-    const dateSignup = Date.UTC;
-    if(pass == req.body.confirmPassword && pass.length <= 6){
+    
+    const dateSignup = Date.now();
+    if(pass === req.body.confirmPassword){
         
-        const firebaseRef = firebase.database().ref().child('users');
-        
-        firebaseRef.child(storename).set({
+        const firebaseRef = db.ref('users');
+        const storageRef= firebase.storage().ref('files/'+storename+'/'+'savage.png').put(files[0]);
+
+             firebaseRef.child(storename).set({
             FirstName: firstname,
             LastName: lastname,
             Password: pass,
             Email: email,
             telephone: tele,
-            StoreName: storename
+            StoreName: storename,
+            date: dateSignup
         });
-
-        firebaseRef.child(storename).push();
 
        const promise = auth.createUserWithEmailAndPassword(email, pass);
        promise.catch(e => console.log(e.message));
+
+        
     }
 
     else {
